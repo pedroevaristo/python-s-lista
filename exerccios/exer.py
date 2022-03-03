3fd595e1835259b4a73aee5e2f04274288211146
pedroevaristo <hattoriminamoto@gmail.com>
2022-03-03 19:58:22 -0300

    3222

---
+++ exerccios/exer.py
@@ -0,0 +1,8 @@
+print('Digite 3 valores:')
+a = int (input())
+b = int (input())
+c = int (input())
+print(a + b + c / 3)#funcionou:)
+print(a*b*c)
+print(a-b-c)
+print(a*c/b)
---
+++ exerccios/exer5.py
@@ -0,0 +1,34 @@
+#Switch-case
+import math
+print('Digite apartir de dois digitos: ')
+n = int(input('Digite um numero'))
+m = int(input('Digite um numero'))
+v = int(input('Digite um numero'))
+x = int(input('Digite um numero'))
+
+# usa este caramba com if-else mesmo
+
+t = n*n
+t1 = m*m
+t2 = v*v
+t3 = x*x
+
+if t >= 1000:
+    print(t)
+elif t <= 1000:
+    print('Eh menor que 1000')
+    #
+if t1 >= 1000:
+    print(t1)
+elif t1 <= 1000:
+    print('Eh menor que 1000')
+    #
+if t2 >= 1000:
+    print(t2)
+elif t2 <= 1000:
+    print('Eh menor que 1000')
+    #
+if t3 >= 1000:
+    print(t3)
+elif t3 <= 1000:
+    print('Eh menor que 1000')
